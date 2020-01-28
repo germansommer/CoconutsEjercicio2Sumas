3b6c7a68aea93ebe960d9942140131639dc5a98a
germansommer <germangsommer@gmail.com>
2020-01-28 11:55:52 -0300

    Add files via upload

---
+++ CocoSumas02/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace CocoSumas02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaracion de Variables
+            int num1 = 0, num2= 0, res = 0, i;
+            do
+            {
+                Console.WriteLine("Ingrese el primer numero: ");
+                try
+                {
+                    num1 = int.Parse(Console.ReadLine());
+                }
+                catch (FormatException) { }
+            } while (num1 < 1);
+            do
+            {
+                Console.WriteLine("Ingrese el segundo numero: ");
+                try
+                {
+                    num2 = int.Parse(Console.ReadLine());
+                }
+                catch (FormatException) { }
+            } while (num2 < 1);            
+            Console.WriteLine("******************************");            
+            for (i = 0; i < num2; i++)
+            {
+                res += num1;
+                Console.WriteLine(num1 + "x" + (i+1) + "= " + res);
+            }
+        }
+    }
+}
